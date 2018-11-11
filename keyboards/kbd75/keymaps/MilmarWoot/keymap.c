27d3a1d9aa217de3a64b7d0d20f562d6c25aff1c
Konstantin Đorđević <vomindoraan@gmail.com>
2018-11-11 15:22:42 +0100

    Update keymap.c

--- keyboards/kbd75/keymaps/MilmarWoot/keymap.c
+++ keyboards/kbd75/keymaps/MilmarWoot/keymap.c
@@ -1,69 +1,77 @@
 #include QMK_KEYBOARD_H
 
-#define _______ KC_TRNS
-#define LCTLL LCTL(KC_L)
+#define LOCK TG(L_LOCK)
+#define FN   MO(L_FN)
+
+#define LCTLL  LCTL(KC_L)
 #define LGUILE LGUI(KC_LEFT)
 #define LGUIRI LGUI(KC_RIGHT)
 #define LGUIUP LGUI(KC_UP)
 #define LGUIDO LGUI(KC_DOWN)
 
+#define CAPS_BIT (1 << 2)
+
+enum layers {
+  L_BASE,
+  L_LOCK,
+  L_FN,
+}
+
 enum custom_keycodes {
-	M_GIT = SAFE_RANGE,
-	M_4SPC
+  M_GIT = SAFE_RANGE,
+  M_4SPC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-	[0] = LAYOUT(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_HOME,  KC_END,   KC_INS,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_DEL,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
-    KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    M_GIT,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   M_4SPC,   LCTLL,                        KC_RALT,  MO(2),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+  [L_BASE] = LAYOUT( \
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_INS,  \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_DEL,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP, \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,  KC_PGDN, \
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   M_GIT,   \
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  M_4SPC,  LCTLL,                     KC_RALT, FN,      KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
-	[1] = LAYOUT(
-			KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-		  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-		  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,
-		  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_NO,
-		  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,              KC_NO,    KC_NO,
-		  KC_NO,    KC_NO,    KC_NO,                        KC_NO,    KC_NO,    KC_NO,                        KC_NO,    MO(2),    KC_NO,   KC_NO,     KC_NO,    KC_NO
-	),
-
-	[2] = LAYOUT(
-      RESET,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(0),
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______,  _______,  _______,  _______,            _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,
-    _______,  _______,  _______,  _______,  BL_DEC,   BL_TOGG,  BL_INC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,             LGUIUP,  _______,
-    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  MO(1),    _______,  LGUILE,    LGUIDO,  LGUIRI
+  [L_LOCK] = LAYOUT( \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, FN,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  \
   ),
 
-
+  [L_FN] = LAYOUT( \
+    RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LOCK,    \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,          _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, \
+    _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______, _______, _______, _______,          LGUIUP,  _______, \
+    _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, LGUILE,  LGUIDO,  LGUIRI   \
+  ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	if (record->event.pressed) {
-		switch(keycode) {
-			case M_GIT:
-				SEND_STRING("git shorty"SS_TAP(X_ENTER));
-				return false;
-			case M_4SPC:
-				SEND_STRING("    ");
-				return false;
-			default :
-			 return true;
-		}
-	}
-	return true;
-};
+  if (record->event.pressed) {
+    switch(keycode) {
+    case M_GIT:
+      SEND_STRING("git shorty" SS_TAP(X_ENTER));
+      return false;
+    case M_4SPC:
+      SEND_STRING("    ");
+      return false;
+    default:
+      return true;
+    }
+  }
+}
 
 void led_set_user(uint8_t usb_led) {
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		DDRB |= (1 << 2); PORTB &= ~(1 << 2);
-	} else {
-		DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-	}
+  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+    DDRB  |=  CAPS_BIT;
+    PORTB &= ~CAPS_BIT;
+  } else {
+    DDRB  &= ~CAPS_BIT;
+    PORTB &= ~CAPS_BIT;
+  }
 };
