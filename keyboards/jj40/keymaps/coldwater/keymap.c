1061de85cb3ab6465624dc73d3f6d00e3a71c338
Spaceman Spiff <coldwater.yu@gmail.com>
2018-12-16 12:49:33 +0100

    add new defines, tweak layout

--- keyboards/jj40/keymaps/coldwater/keymap.c
+++ keyboards/jj40/keymaps/coldwater/keymap.c
@@ -32,6 +32,15 @@ enum custom_keycodes {
 #define DIVVY LALT(LGUI(LSFT(KC_D)))
 #define F_CHR LALT(LSFT(KC_E))
 #define F_WIN LALT(LGUI(LSFT(KC_W)))
+#define MEDIA_PREV KC_MEDIA_PREV_TRACK
+#define MEDIA_NEXT KC_MEDIA_NEXT_TRACK
+#define MEDIA_PLAY KC_MEDIA_PLAY_PAUSE
+#define MEDIA_STOP KC_MEDIA_STOP
+#define MEDIA_VOLUP KC_AUDIO_VOL_UP
+#define MEDIA_VOLDN KC_AUDIO_VOL_DOWN
+
+
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -43,17 +52,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-  _______,  _______,  _______,  _______,  KC_GRAVE,   _______,      _______,      KC_7,     KC_8, KC_9,   KC_MINUS, KC_HOME, \
-  _______,  _______,  DIVVY,    F_CHR,    F_WIN,      KC_LPRN,      KC_RPRN,      KC_4,     KC_5, KC_6,   KC_EQUAL, KC_PGUP, \
-  _______,  _______,  _______,  _______,  KC_PSCREEN, KC_LBRACKET,  KC_RBRACKET,  KC_1,     KC_2, KC_3,   KC_SLSH,  KC_PGDOWN, \
-  _______,  _______,  _______,  _______,  _______,    _______,      _______,      _______,  KC_0, KC_DOT, _______,  KC_END \
+  _______,  _______,  _______,  _______,  KC_GRAVE,   _______,      _______,      KC_7, KC_8,   KC_9,   KC_EQUAL, KC_HOME, \
+  _______,  _______,  DIVVY,    F_CHR,    F_WIN,      KC_LPRN,      KC_RPRN,      KC_4, KC_5,   KC_6,   KC_MINUS, KC_PGUP, \
+  _______,  _______,  _______,  _______,  KC_PSCREEN, KC_LBRACKET,  KC_RBRACKET,  KC_1, KC_2,   KC_3,   KC_SLSH,  KC_PGDOWN, \
+  _______,  _______,  _______,  _______,  _______,    _______,      _______,      KC_0, KC_DOT, KC_DOT, _______,  KC_END \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-	KC_F1,	  KC_F2,	  KC_F3,	        KC_F4,	    KC_F5,	        KC_F6,	        KC_F7,	  KC_F8,	              KC_F9,	              KC_F10,	            KC_F11,	              KC_F12	,	\
-	RGB_TOG,	RGB_MOD,	KC_MS_WH_LEFT,  KC_MS_UP,   KC_MS_WH_RIGHT, KC_MS_WH_UP,    _______,	KC_MS_BTN1,           KC_MS_BTN3,           KC_MS_BTN2,         LALT(LSFT(KC_UP)),    KC_MS_ACCEL2,	\
-	BL_TOGG,	BL_STEP,	KC_MS_LEFT,     KC_MS_DOWN, KC_MS_RIGHT,    KC_MS_WH_DOWN,	_______,	KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_STOP,        KC_APPLICATION,     LALT(LSFT(KC_DOWN)),  KC_MS_ACCEL0,	\
-	_______,	_______,	_______,	      _______,	  _______,	      _______,        _______,	_______,	            KC_MEDIA_PREV_TRACK,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP,      KC_MEDIA_NEXT_TRACK	\
+	KC_F1,	  KC_F2,	  KC_F3,	        KC_F4,	    KC_F5,	        KC_F6,	        KC_F7,	  KC_F8,      KC_F9,      KC_F10,         KC_F11,       KC_F12	,	\
+	RGB_TOG,	RGB_MOD,	KC_MS_WH_LEFT,  KC_MS_UP,   KC_MS_WH_RIGHT, KC_MS_WH_UP,    _______,	KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2,     _______,      KC_MS_ACCEL2,	\
+	BL_TOGG,	BL_STEP,	KC_MS_LEFT,     KC_MS_DOWN, KC_MS_RIGHT,    KC_MS_WH_DOWN,	_______,	MEDIA_PLAY, MEDIA_STOP, KC_APPLICATION, _______,      KC_MS_ACCEL0,	\
+	_______,	_______,	_______,	      _______,	  _______,	      _______,        _______,	_______,    MEDIA_PREV, MEDIA_VOLDN,    MEDIA_VOLUP,  MEDIA_NEXT	\
 )
 };
 
