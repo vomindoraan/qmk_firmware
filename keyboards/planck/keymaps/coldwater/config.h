32486fa83d194e9fc52a94736bc133aeb2214e2b
Milan Stanojevic <milan.stanojevic@tradecore.com>
2019-09-02 15:41:00 +0200

    tweak mouse settings

--- keyboards/planck/keymaps/coldwater/config.h
+++ keyboards/planck/keymaps/coldwater/config.h
@@ -44,6 +44,6 @@
 
 #define MOUSEKEY_INTERVAL       16
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_TIME_TO_MAX    30
 #define MOUSEKEY_MAX_SPEED      4
 #define MOUSEKEY_WHEEL_DELAY 0
