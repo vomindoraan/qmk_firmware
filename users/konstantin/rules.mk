455856f7f8d98b3ef84b50f9861214a7f062acf9
Konstantin Đorđević <vomindoraan@gmail.com>
2021-09-08 02:59:24 +0200

    Consolidate konstantin rules.mk defaults from keymaps into userspace

---
+++ users/konstantin/rules.mk
@@ -0,0 +1,19 @@
+# Generic features
+BOOTMAGIC_ENABLE   = no
+COMMAND_ENABLE     = yes
+CONSOLE_ENABLE     = yes
+EXTRAKEY_ENABLE    = yes
+MOUSEKEY_ENABLE    = yes
+NKRO_ENABLE        = yes
+TAP_DANCE_ENABLE   = yes
+UNICODEMAP_ENABLE  = yes
+
+# Keyboard-specific feature defaults
+BACKLIGHT_ENABLE   = no
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = no
+
+# Firmware size reduction
+GRAVE_ESC_ENABLE   = no
+MAGIC_ENABLE       = no
+SPACE_CADET_ENABLE = no
